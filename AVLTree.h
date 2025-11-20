8a2838aef8a53e5ffb1f64b51f49a8231ea7b14a
Lucas-Sperati <sperati.2@wright.edu>
2025-11-19 19:41:27 -0500

    trying to get debug to run

--- AVLTree.h
+++ AVLTree.h
@@ -15,6 +15,25 @@ public:
     using KeyType = std::string;
     using ValueType = size_t;
 
+    AVLTree();
+    ~AVLTree();
+    bool remove(const std::string &key);
+    bool insert(const std::string &key, size_t value);
+    bool contains(const std::string &key) const;
+    std::optional<size_t> get(const std::string &key) const;
+    size_t &operator[](const std::string &key);
+    vector<std::string> findRange(const std::string &lowKey, const std::string &highKey) const;
+    std::vector<std::string> keys() const;
+    size_t size() const;
+    size_t getHeight() const;
+    AVLTree(const AVLTree &other);
+    void operator=(const AVLTree &other);
+
+    //prints out the tree
+    friend std::ostream &operator<<(std::ostream &os, const AVLTree &avlTree);
+
+
+
 protected:
     class AVLNode {
     public:
@@ -50,52 +69,23 @@ public:
 
     //---------------------------------------------------------------------------------------------------------
 
-    bool insert(const std::string &key, size_t value);
-
     void insertPart2(AVLNode *parent, AVLNode *newNode);
-
-    bool remove(const std::string &key);
-
-    bool contains(const std::string &key) const;
 
     bool contains2(const AVLNode *current, const KeyType &key) const;
 
-    std::optional<size_t> get(const std::string &key) const;
-
     std::optional<size_t> get2(AVLNode *current, KeyType key) const;
-
-    size_t &operator[](const std::string &key);
 
     size_t &operator2(AVLNode *&current, KeyType key);
 
-    vector<std::string> findRange(const std::string &lowKey, const std::string &highKey) const;
-
     void findRange2(AVLNode *current, const std::string &lowKey, const std::string &highKey, vector<string> &keysVector) const;
-
-    std::vector<std::string> keys() const;
 
     void keys2(AVLNode *current, vector<string> &keysVector) const;
 
-    size_t size() const;
-
-    size_t getHeight() const;
-
-    AVLTree(const AVLTree &other);
-
     AVLNode *copyConstucter(const AVLNode *current);
-
-    void operator=(const AVLTree &other);
-
-    ~AVLTree();
 
     void deconstructorMethod(AVLNode *current);
 
-    AVLTree();
-
     void printMethod(std::ostream &os, AVLNode *current) const;
-
-    //prints out the tree
-    friend std::ostream &operator<<(std::ostream &os, const AVLTree &avlTree);
 };
 
 #endif //AVLTREE_H
